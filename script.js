19cee79714f68b3cb2416b274f5bfce7effeebcf
Sofia7b <sofiabelicenko292@gmail.com>
2025-07-10 23:35:43 +0300

    took out the js into a separate file

---
+++ script.js
@@ -0,0 +1,10 @@
+const table = document.getElementById("pifagor");
+for (let i = 1; i <= 10; i++) {
+  const row = document.createElement("tr");
+  for (let j = 1; j <= 10; j++) {
+    const cell = document.createElement("td");
+    cell.textContent = i * j;
+    row.appendChild(cell);
+  }
+  table.appendChild(row);
+}
